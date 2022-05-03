e6e661b972c8d5d62c5f6187bb07e58a0e11e89b
Park SungHwan <park0219ok@outlook.com>
2022-05-03 23:23:29 +0900

    feat: User, Authority Entity 추가

---
+++ src/main/java/me/park/tildiaryspringboot/entity/User.java
@@ -0,0 +1,43 @@
+package me.park.tildiaryspringboot.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "username", length = 50, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "nickname", length = 50)
+    private String nickname;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "email_receives")
+    private boolean email_receives;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+}
