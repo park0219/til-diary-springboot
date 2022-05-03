6a006126a0d391e2a7e0055b5d16424567998bbc
Park SungHwan <park0219ok@outlook.com>
2022-05-03 22:51:55 +0900

    Initial commit

---
+++ src/main/java/me/park/tildiaryspringboot/TilDiarySpringbootApplication.java
@@ -0,0 +1,13 @@
+package me.park.tildiaryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilDiarySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilDiarySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/park/tildiaryspringboot/TilDiarySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.park.tildiaryspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilDiarySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
