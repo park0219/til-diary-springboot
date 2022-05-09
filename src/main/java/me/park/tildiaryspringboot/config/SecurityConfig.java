8d254afc8d9c6c00050e2710830b6ca394b9b8de
Park SungHwan <park0219ok@outlook.com>
2022-05-10 00:14:16 +0900

    feat: CORS 설정 추가

--- src/main/java/me/park/tildiaryspringboot/config/SecurityConfig.java
+++ src/main/java/me/park/tildiaryspringboot/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import me.park.tildiaryspringboot.jwt.JwtAccessDeniedHandler;
 import me.park.tildiaryspringboot.jwt.JwtAuthenticationEntryPoint;
 import me.park.tildiaryspringboot.jwt.JwtSecurityConfig;
 import me.park.tildiaryspringboot.jwt.TokenProvider;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 //@PreAuthorize 어노테이션을 메소드 단위로 추가하기 위해 적용
@@ -22,11 +24,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CorsFilter corsFilter;
 
-    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler, CorsFilter corsFilter) {
         this.tokenProvider = tokenProvider;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.corsFilter = corsFilter;
     }
 
     //PasswordEncoder 빈
@@ -40,6 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 //token을 사용하는 방식이기 때문에 csrf를 disable합니다.
                 .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
